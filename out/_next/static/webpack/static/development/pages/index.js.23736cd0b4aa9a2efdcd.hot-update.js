787ecb0a9e13f9310c5cda9440f721be115dd4f3
Jordan Salazar <jrsalazar@cirkel.cc>
2019-12-03 20:23:11 +0100

    next exported to out folder

---
+++ out/_next/static/webpack/static/development/pages/index.js.23736cd0b4aa9a2efdcd.hot-update.js
@@ -0,0 +1,61 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/DownloadOrWebButton.js":
+/*!*******************************************!*\
+  !*** ./components/DownloadOrWebButton.js ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral */ "./node_modules/@babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _emotion_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @emotion/core */ "./node_modules/@emotion/core/dist/core.browser.esm.js");
+/* harmony import */ var _emotion_styled__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @emotion/styled */ "./node_modules/@emotion/styled/dist/styled.browser.esm.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_4__);
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement;
+
+function _templateObject() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__["default"])(["\n      & > .download-btn,\n      & > .view-online-btn {\n        position: absolute;\n        top: 40px;\n        right: 50px;\n        font-size: 30px;\n\n        & > a {\n          color: black;\n        }\n      }\n\n      & > .view-online-btn {\n        display: none;\n      }\n\n      @media print {\n        & > .download-btn {\n          display: none;\n        }\n\n        & > .view-online-btn {\n          display: block;\n          right: 0;\n        }\n      }\n    "]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+/** @jsx jsx */
+
+
+
+
+var DownloadOrWebButton = function DownloadOrWebButton(props) {
+  return Object(_emotion_core__WEBPACK_IMPORTED_MODULE_2__["jsx"])("div", {
+    css: Object(_emotion_core__WEBPACK_IMPORTED_MODULE_2__["css"])(_templateObject())
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_2__["jsx"])("div", {
+    className: "download-btn"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_2__["jsx"])(next_link__WEBPACK_IMPORTED_MODULE_4___default.a, {
+    href: "/pdf"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_2__["jsx"])("a", null, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_2__["jsx"])("i", {
+    className: "ion-android-download"
+  })))), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_2__["jsx"])("div", {
+    className: "view-online-btn"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_2__["jsx"])(next_link__WEBPACK_IMPORTED_MODULE_4___default.a, {
+    href: "/"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_2__["jsx"])("i", {
+    className: "ion-android-open"
+  }))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (DownloadOrWebButton);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.23736cd0b4aa9a2efdcd.hot-update.js.map
---
+++ out/_next/static/webpack/static/development/pages/index.js.682d126774394f4a8070.hot-update.js
@@ -0,0 +1,146 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Index; });
+/* harmony import */ var _babel_runtime_corejs2_core_js_date_now__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/core-js/date/now */ "./node_modules/@babel/runtime-corejs2/core-js/date/now.js");
+/* harmony import */ var _babel_runtime_corejs2_core_js_date_now__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_corejs2_core_js_date_now__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral */ "./node_modules/@babel/runtime-corejs2/helpers/esm/taggedTemplateLiteral.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _emotion_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @emotion/core */ "./node_modules/@emotion/core/dist/core.browser.esm.js");
+/* harmony import */ var _components_layout_ContentFlexContainer__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../components/layout/ContentFlexContainer */ "./components/layout/ContentFlexContainer.js");
+/* harmony import */ var _components_layout_ContentContainer__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../components/layout/ContentContainer */ "./components/layout/ContentContainer.js");
+/* harmony import */ var _components_layout_Background__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../components/layout/Background */ "./components/layout/Background.js");
+/* harmony import */ var _components_layout_Section__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../components/layout/Section */ "./components/layout/Section.js");
+/* harmony import */ var _components_layout_Page__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../components/layout/Page */ "./components/layout/Page.js");
+/* harmony import */ var _components_layout_Head__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ../components/layout/Head */ "./components/layout/Head.js");
+/* harmony import */ var _components_SectionHead__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ../components/SectionHead */ "./components/SectionHead.js");
+/* harmony import */ var _components_typography_PageTitle__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ../components/typography/PageTitle */ "./components/typography/PageTitle.js");
+/* harmony import */ var _components_timeline__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ../components/timeline */ "./components/timeline/index.js");
+/* harmony import */ var _components_UnorderedList__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ../components/UnorderedList */ "./components/UnorderedList.js");
+/* harmony import */ var _components_SkillsItem__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ../components/SkillsItem */ "./components/SkillsItem.js");
+/* harmony import */ var _components_Profile__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(/*! ../components/Profile */ "./components/Profile.js");
+/* harmony import */ var _components_DownloadOrWebButton__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(/*! ../components/DownloadOrWebButton */ "./components/DownloadOrWebButton.js");
+/* harmony import */ var _components_typography__WEBPACK_IMPORTED_MODULE_17__ = __webpack_require__(/*! ../components/typography */ "./components/typography/index.js");
+
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement;
+
+function _templateObject3() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_1__["default"])(["\n                display: flex;\n                flex-wrap: wrap;\n              "]);
+
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject2() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_1__["default"])(["\n                width: 600px;\n                ", ";\n              "]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject() {
+  var data = Object(_babel_runtime_corejs2_helpers_esm_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_1__["default"])(["\n                  margin-bottom: 5rem;\n\n                  @media print {\n                    margin-bottom: 10px;\n                  }\n                "]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+/** @jsx jsx */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Index() {
+  return Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_Background__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    topography: true
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_Head__WEBPACK_IMPORTED_MODULE_9__["default"], null), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_Page__WEBPACK_IMPORTED_MODULE_8__["default"], null, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_Section__WEBPACK_IMPORTED_MODULE_7__["default"], {
+    className: "splash"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_ContentFlexContainer__WEBPACK_IMPORTED_MODULE_4__["default"], null, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_ContentContainer__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    flex: "2.5"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_DownloadOrWebButton__WEBPACK_IMPORTED_MODULE_16__["default"], null), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_SectionHead__WEBPACK_IMPORTED_MODULE_10__["default"], {
+    icon: "ion-android-contact",
+    title: "Profile"
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography_PageTitle__WEBPACK_IMPORTED_MODULE_11__["default"], null, "Jordan Salazar"), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography__WEBPACK_IMPORTED_MODULE_17__["Heading05"], {
+    css: Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["css"])(_templateObject())
+  }, "Full time Webmagician, part time human being"), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography__WEBPACK_IMPORTED_MODULE_17__["Paragraph"], null, "I'm a ", ~~((_babel_runtime_corejs2_core_js_date_now__WEBPACK_IMPORTED_MODULE_0___default()() - +new Date(1992, 9, 31)) / 31557600000), ' ', "year old webdeveloper living in Amsterdam, born in the US. Looking to work freelance, build awesome products and utilise all the modern tools available.")), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_ContentContainer__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    flex: "1"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_Profile__WEBPACK_IMPORTED_MODULE_15__["default"], null)))), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_Section__WEBPACK_IMPORTED_MODULE_7__["default"], {
+    className: "skillset"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_ContentContainer__WEBPACK_IMPORTED_MODULE_5__["default"], null, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_SectionHead__WEBPACK_IMPORTED_MODULE_10__["default"], {
+    icon: "ion-ios-flask-outline",
+    title: "Skillset"
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography__WEBPACK_IMPORTED_MODULE_17__["Paragraph"], null, "I'm a driven craftsman, specialised in architecting and developing products from concept to MVP and beyond. Future-proofing projects with the best practices of today such as Automated Testing (BDD/TDD), Continuous Integration, Continuous Delivery and Infrastructure Monitoring. With a passion for finding the right tools for the job."), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography__WEBPACK_IMPORTED_MODULE_17__["Heading04"], null, "Primary skills"), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_UnorderedList__WEBPACK_IMPORTED_MODULE_13__["default"], {
+    css: Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["css"])(_templateObject2(), _components_typography__WEBPACK_IMPORTED_MODULE_17__["paragraph"])
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])("li", {
+    className: "ion-ios-world"
+  }, "Orchestrating and developing advanced web applications and (micro-)services (Laravel,Node.js)"), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])("li", {
+    className: "ion-network"
+  }, "Creating and connecting seperate software systems through API's and managing data flows (REST,GraphQL,Web/Sockets,SOAP)"), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])("li", {
+    className: "ion-ios-monitor"
+  }, "Engineering next-gen user interfaces with Javascript and modern web technologies. (ES2020,React,Redux,Vue,Vuex,CSS-in-JS,SCSS)"), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])("li", {
+    className: "ion-iphone"
+  }, "Developing and maintaining cross platform mobile apps (React Native,Ionic)"), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])("li", {
+    className: "ion-code-working"
+  }, "Setting up professional engineering pipelines (Bash,Docker,CI,CD,Puppet)")), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography__WEBPACK_IMPORTED_MODULE_17__["Heading04"], null, "Languages & Frameworks"), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])("div", {
+    css: Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["css"])(_templateObject3())
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_SkillsItem__WEBPACK_IMPORTED_MODULE_14__["default"], {
+    title: "Javascript",
+    icon: "ion-social-javascript-outline",
+    tags: "React,Redux,Vue,Vuex,Angular.js,Angular,RxJS,Express,Hapi",
+    level: "5"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography__WEBPACK_IMPORTED_MODULE_17__["Paragraph"], null, "JS is my oldest friend and the go-to language for anything general purpose. Started my career using Angular.js and SCSS, these days I'm all about component based architectures, unidirectional data flow and CSS-in-JS."), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography__WEBPACK_IMPORTED_MODULE_17__["Paragraph"], null, "On the server I've set up microservices and monoliths. REST API's and socket based messaging endpoints. Written scripts to combine and manage frontend assets (pre-webpack) and developed things as a video-overlay generator with node-ffmpeg.")), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_SkillsItem__WEBPACK_IMPORTED_MODULE_14__["default"], {
+    title: "PHP: Hypertext Preprocessor",
+    image: "/img/php-512.png",
+    tags: "Laravel,Symfony,PHPUnit,DependencyInjection",
+    level: "5"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography__WEBPACK_IMPORTED_MODULE_17__["Paragraph"], null, "PHP (Namely Laravel) is the first choice for cut and clear web applications where an opinionated framework is the first choice. Intimate with Laravel's best practices and feature rich ecosystem.")), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_SkillsItem__WEBPACK_IMPORTED_MODULE_14__["default"], {
+    tags: "Emotion,styled-components,SCSS",
+    title: "CSS",
+    icon: "ion-social-css3-outline",
+    level: "4"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography__WEBPACK_IMPORTED_MODULE_17__["Paragraph"], null, "Whether you're building static pages or component based javascript apps, CSS is an essential player. Mastered most of its inter-browser quirks, animation based complexities and best practices for setting up and managing large amounts of css.")), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_SkillsItem__WEBPACK_IMPORTED_MODULE_14__["default"], {
+    title: "GNU + Linux",
+    tags: "ArchLinux,Debian,Alpine,bash,zsh,fish,Docker,Git,Vim",
+    image: "/img/Heckert_GNU_white.svg",
+    level: "3"
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_typography__WEBPACK_IMPORTED_MODULE_17__["Paragraph"], null, "Full time GNU + Linux user for 3-4 years. When working with thousands of text files, libraries and packages, theres no better environment to do what you need. Confident writing deploy scripts, managing multiple servers and debugging system related issues."))))), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_Section__WEBPACK_IMPORTED_MODULE_7__["default"], null, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_layout_ContentContainer__WEBPACK_IMPORTED_MODULE_5__["default"], null, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_SectionHead__WEBPACK_IMPORTED_MODULE_10__["default"], {
+    icon: "ion-ios-infinite-outline",
+    title: "Timeline"
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_3__["jsx"])(_components_timeline__WEBPACK_IMPORTED_MODULE_12__["default"], null)))));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.682d126774394f4a8070.hot-update.js.map
