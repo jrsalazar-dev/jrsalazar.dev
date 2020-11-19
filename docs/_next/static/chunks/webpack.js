c14e4265195c2945e55d1358277ed76b06e63396
jrsalazar <contact@jrsalazar.dev>
2020-11-19 16:48:05 +0100

    deployed to docs

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9adb5d0ae3fc08baaea4";
+/******/ 	var hotCurrentHash = "c58a0514714e7385e500";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ docs/_next/static/mpryI9ZQnXl0uxTbhaWIM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e518ef2111e5a726ad1e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4608ace64cdc391e2902.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
